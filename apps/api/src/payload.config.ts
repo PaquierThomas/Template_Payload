5ee1158841427b4af449e1f7ef56a34251bcc360
PaquierThomas <thomas.paquier.pro@gmail.com>
2026-01-19 14:21:40 +0100

    push

--- apps/api/src/payload.config.ts
+++ apps/api/src/payload.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -11,6 +12,8 @@ import  Posts  from "./collections/Posts";
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+// First, set a variable to check if we're in the build phase
+const isBuild = process.env.BUILD === "true"
 
 export default buildConfig({
   admin: {
@@ -27,9 +30,26 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString: 
+        isBuild || process.env.NODE_ENV === 'development'
+          ? process.env.BUILD_DATABASE || ''
+          : process.env.DATABASE_URL || '',
     },
   }),
   sharp,
-  plugins: [],
+  plugins: [s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.AWS_S3_BUCKET_NAME || '',
+      config: {
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+        },
+        region: process.env.AWS_DEFAULT_REGION,
+        // Important: force path style so images work on the frontend
+        forcePathStyle: true,
+      },
+    }),],
 })
